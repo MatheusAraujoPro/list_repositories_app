7682708299c5d80c9701faaf80bd40df1090f8be
Matheus <matheusaraujo.ti10@gmail.com>
2022-08-05 22:30:25 -0300

    feat: add loader to Screan

--- app/src/main/java/com/meuapp/listrepositoriesapp/presentation/repolist/RepoListView.kt
+++ app/src/main/java/com/meuapp/listrepositoriesapp/presentation/repolist/RepoListView.kt
@@ -1,49 +1,64 @@
 package com.meuapp.listrepositoriesapp.presentation.repolist
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import kotlin.coroutines.coroutineContext
 
 @Composable
-fun RepoListView(vm: RepoListViewModel){
+fun RepoListView(vm: RepoListViewModel) {
     /***
-     * Assim que a tela carrega, ele chama o 
+     * Assim que a tela carrega, ele chama o
      * método getRepos da ViewModel
      */
-    val coroutineScope = rememberCoroutineScope()
-
     LaunchedEffect(Unit, block = {
-        val list = vm.getRepos()
-
+        vm.getRepos()
     })
-    
+
     Scaffold(
         topBar = {
             TopAppBar(title = {
                 Text(text = "My Repositories")
-            } ) 
-                        
+            })
+
         },
         content = {
-            Column(modifier = Modifier.padding(16.dp)) {
+            Column(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = CenterHorizontally
+
+
+            ) {
                 // Trabalhando com listas
-                LazyColumn(modifier = Modifier.fillMaxSize()){
-                    //Pegando o item da Iteração
-                    items(vm.repos){ repo ->
-                        RepoItem(repo = repo)
+                if (vm.repos.isEmpty()) {
+                    CircularProgressIndicator()
+
+                } else {
+                    // loadedList = vm.repos.size
+                    LazyColumn() {
+                        //Pegando o item da Iteração
+                        items(vm.repos) { repo ->
+                            RepoItem(repo = repo)
+                        }
                     }
+
                 }
             }
         }
