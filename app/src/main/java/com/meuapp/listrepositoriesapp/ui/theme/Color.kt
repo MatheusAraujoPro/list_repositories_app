7e53571d4c90a5957391e79c0ad088ad5ee9f07a
Matheus <matheusaraujo.ti10@gmail.com>
2022-08-07 11:56:49 -0300

    feat: New theme created

--- app/src/main/java/com/meuapp/listrepositoriesapp/ui/theme/Color.kt
+++ app/src/main/java/com/meuapp/listrepositoriesapp/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.meuapp.listrepositoriesapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
+val GitHubBlack = Color(0xFF0d1117)
+val Purple500 = Color(  0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
