f2cc6a32c1effd4df25b8793f8bd3e97a62dc86a
Matheus <matheusaraujo.ti10@gmail.com>
2022-08-07 11:58:26 -0300

    refactor: Using new theme

--- app/src/main/java/com/meuapp/listrepositoriesapp/presentation/repolist/RepoListView.kt
+++ app/src/main/java/com/meuapp/listrepositoriesapp/presentation/repolist/RepoListView.kt
@@ -7,10 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
@@ -31,9 +28,11 @@ fun RepoListView(vm: RepoListViewModel) {
 
     Scaffold(
         topBar = {
-            TopAppBar(title = {
-                Text(text = "My Repositories")
-            })
+            TopAppBar(
+                backgroundColor = MaterialTheme.colors.primary,
+                title = {
+                    Text(text = "My Repositories")
+                })
 
         },
         content = {
