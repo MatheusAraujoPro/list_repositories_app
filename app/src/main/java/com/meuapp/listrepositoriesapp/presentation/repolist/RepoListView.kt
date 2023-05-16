399d399929eba6d3a96db3576fb6fb5c70114a10
Matheus Araújo <matheus.a@havan.com.br>
2023-05-16 16:46:56 -0300

    chore: Ajusta lazyColounm

--- app/src/main/java/com/meuapp/listrepositoriesapp/presentation/repolist/RepoListView.kt
+++ app/src/main/java/com/meuapp/listrepositoriesapp/presentation/repolist/RepoListView.kt
@@ -1,22 +1,33 @@
 package com.meuapp.listrepositoriesapp.presentation.repolist
 
-import android.util.Log
-import androidx.compose.foundation.layout.*
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.meuapp.listrepositoriesapp.domain.model.Repo
 
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun RepoListView(vm: RepoListViewModel) {
     var isAppBarSearchVisibility by remember { mutableStateOf(false) }
     var text by remember { mutableStateOf("") }
     var listReposOnScrean by remember { mutableStateOf(listOf<Repo>()) }
+
 
     fun handleBarVisibility(): Unit {
         isAppBarSearchVisibility = !isAppBarSearchVisibility
