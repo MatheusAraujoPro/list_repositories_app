13a5f0cc5706f513cf22ca50bd2f5b482329ee57
Matheus Araújo <matheus.a@havan.com.br>
2023-05-17 13:06:20 -0300

    chore: Retira lógica de chamada à APi e refatora código

--- app/src/main/java/com/meuapp/listrepositoriesapp/data/services/ReposWebService.kt
+++ app/src/main/java/com/meuapp/listrepositoriesapp/data/services/ReposWebService.kt
@@ -1,34 +1,10 @@
 package com.meuapp.listrepositoriesapp.data.services
 
 import com.meuapp.listrepositoriesapp.data.models.RepoEntity
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import com.meuapp.listrepositoriesapp.domain.constants.RepoConstants
 import retrofit2.http.GET
 
 interface ReposWebService {
-
-    @GET("users/MatheusAraujoPro/repos")
+    @GET(RepoConstants.WebConstants.GITHUB_URL_USER_REPO)
     suspend fun getRepos(): List<RepoEntity>
-
-    companion object {
-        var repoWebService: ReposWebService? = null
-        fun getInstance(): ReposWebService {
-            if (repoWebService == null) {
-                val logging = HttpLoggingInterceptor()
-                logging.setLevel(HttpLoggingInterceptor.Level.BODY)
-
-                val client = OkHttpClient().newBuilder()
-                client.addInterceptor(logging)
-
-                repoWebService = Retrofit.Builder()
-                    .baseUrl("https://api.github.com/")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .client(client.build())
-                    .build().create(ReposWebService::class.java)
-            }
-            return repoWebService!!
-        }
-    }
 }
