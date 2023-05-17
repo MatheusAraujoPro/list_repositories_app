7789566d51f9fcd55c7a3b05f68120a85ee0e4be
Matheus Araújo <matheus.a@havan.com.br>
2023-05-17 13:33:21 -0300

    chore: Remove instância da viewModel na Activity

--- app/src/main/java/com/meuapp/listrepositoriesapp/MainActivity.kt
+++ app/src/main/java/com/meuapp/listrepositoriesapp/MainActivity.kt
@@ -5,18 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Surface
 import com.meuapp.listrepositoriesapp.presentation.repolist.RepoListView
-import com.meuapp.listrepositoriesapp.presentation.repolist.RepoListViewModel
 import com.meuapp.listrepositoriesapp.ui.theme.ListRepositoriesAppTheme
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val repoListViewModel: RepoListViewModel by viewModel()
         setContent {
             ListRepositoriesAppTheme {
                 Surface {
-                    RepoListView(repoListViewModel)
+                    RepoListView()
                 }
             }
         }
